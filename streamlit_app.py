98ec82b323f979bf91b17b46cf9ee37136f29efc
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 14:25:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import altair as alt
+import pandas as pd
+
+# Load the data from excel. We're caching this so it doesn't reload every time the app
+# reruns (e.g. if the user interacts with the widgets).
+@st.cache_data
+def load_data():
+    df = pd.read_excel("data/logbook.xlsx")
+    return df
+
+
+df = load_data()
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+''
+
+st.table(df)
