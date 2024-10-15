7799223cfff77e4ae96bd7aa02e11de9130972d1
bill-climb <billy.forsyth97@gmail.com>
2024-10-15 15:18:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px 
 import plotly.graph_objects as go
+import random
 
 
 # Load the data from excel. We're caching this so it doesn't reload every time the app
